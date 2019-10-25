60ad7a8cf1808c007a7e687cc800634d778634fb
Sean-Williamsgit config --global user.email sean.r.williams85@gmail.comgit config --global user.name Sean-Williams <wilmslo@gmail.com>
2019-10-25 10:52:06 +0100

    FEAT: Create 3 rows of random targets also randomising their size.

--- Project 18c - Milestone - Shooting Gallery/GameScene.swift
+++ Project 18c - Milestone - Shooting Gallery/GameScene.swift
@@ -11,75 +11,59 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var possibleTargets = ["target0", "target1", "target2", "target3"]
+    
+  
     
     override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "wood-background")
+        background.zPosition = -1
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.size = CGSize(width: 1024, height: 768)
+        addChild(background)
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        for i in 0...5 { createTarget(at: CGPoint(x: 100 + (i * 170), y: 210)) }
+        for i in 0...5 { createTarget(at: CGPoint(x: 100 + (i * 170), y: 410)) }
+        for i in 0...5 { createTarget(at: CGPoint(x: 100 + (i * 170), y: 610)) }
+
+    }
+    
+    //MARK: - Private Methods
+    
+    func createTarget(at position: CGPoint) {
+        guard let target = possibleTargets.randomElement() else { return }
+        let sprite = SKSpriteNode(imageNamed: target)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        let scaleSize = Int.random(in: 30...170)
+        sprite.scale(to: CGSize(width: scaleSize, height: scaleSize))
+        sprite.position = position
+        addChild(sprite)
+    }
+    
+    
+    
+    func moveTarget() {
+//        guard let target = possibleTargets.randomElement() else { return }
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
+
+
+    //MARK: - Touch Methods
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
     }
     
     
